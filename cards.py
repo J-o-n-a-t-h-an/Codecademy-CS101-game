fc362c2eee36d28112179220a8dee187d3892cc7
J-o-n-a-t-h-an <66480914+J-o-n-a-t-h-an@users.noreply.github.com>
2022-06-15 12:09:24 -0400

    Update cards.py

--- cards.py
+++ cards.py
@@ -2,6 +2,7 @@ import random
 
 #empty list used to track cards drawn to avoid duplicate cards.
 cards_drawn = []
+# this function is to generate a random card.
 def draw_card():
     # Card components
     #suits = "♠️♣️♥️♦️" - not currently using this but I've left it in because I hope to replace the suit words with the suite emoji.
@@ -12,14 +13,14 @@ def draw_card():
     draw_a_suit = suits[random.randint(0, len(suits)-1)]
     draw_a_name = card_names[random.randint(0, len(card_names)-1)]
 
-    #combined values to make a card
+    #combined values to make the card
     new_card = f"{draw_a_name}-{draw_a_suit}"
     return new_card
 
 # print(f"Computer drew, {draw_card()}")
 
 
-
+# this function asks the user how many cards they need and keeps pulling until it has a unique list equalling the requested number.
 def card_deck():
     hand = input("How many cards would you like?: ")
     #empty variable for the future hand
@@ -32,7 +33,6 @@ def card_deck():
             cards_drawn.append(temp_card)
             card_hand += temp_card
             card_hand += "\n"
-            #card_hand += "\n"
             count += 1
 
     return card_hand
@@ -40,3 +40,4 @@ def card_deck():
 print(card_deck())
 # print(cards_drawn)
 
+#just realized I deleted all the stuff that checks to make sure user is inputing a positive number.
